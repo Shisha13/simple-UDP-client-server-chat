0ababb0540a83c7ba20f6583887b483ce7be7323
Shisha13 <kaktuz.fmc@gmail.com>
2017-07-12 15:35:00 +0300

    Update MyServ.py

--- MyServ.py
+++ MyServ.py
@@ -1,25 +1,25 @@
 import socket
 
 
+
+
 class MyServ:
-    udp_ip = '127.0.0.1'
-    udp_port = 54000
-    fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    fd.bind((udp_ip, udp_port))
-    list_of_clients = set()
+    def __init__(self,ip,port):
+        self._udp_ip = ip
+        self._udp_port = port
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.bind((self._udp_ip, self._udp_port))
+        self._list_of_clients = set()
+
+
 
     def start(self):
         while True:
-            data,client_address = self.fd.recvfrom(1024)
-            self.list_of_clients.add(client_address)
-            print(data)
-            for addr in self.list_of_clients:
-                self.fd.sendto(data, addr)
+            data,client_address = self._sock.recvfrom(1024)
+            self._list_of_clients.add(client_address)
+            for addr in self._list_of_clients:
+                self._sock.sendto(data, addr)
 
 
-
-
-SERVER = MyServ()
-SERVER.start()
 
 
