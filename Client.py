1f6ced9ad595c3b3bded4e850357850e96041dc1
Shisha13 <kaktuz.fmc@gmail.com>
2017-07-12 15:31:50 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,29 +1,30 @@
 import socket
 
-from time import sleep
+
 
 
 
 
 class client:
-    fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_ip = '127.0.0.1'
-    udp_port = 54000
-    Is_Entered = False
-    NickName = ""
+    def __init__(self,ip,port):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._udp_ip = ip
+        self._udp_port = port
+        self._Is_Entered = False
+        self._NickName = ""
+
 
     def Enter_chat(self,some):
-        self.NickName = some
-        self.fd.sendto(self.NickName + " join the chat!", (self.udp_ip, self.udp_port))
-        self.Is_Entered = True
+        self._NickName = some
+        self._sock.sendto(self._NickName + " join the chat!", (self._udp_ip, self._udp_port))
+        self._Is_Entered = True
 
     def send(self,message):
-        self.fd.sendto(self.NickName+": "+ message, (self.udp_ip, self.udp_port))
+        self._sock.sendto(self._NickName + ": " + message, (self._udp_ip, self._udp_port))
 
 
     def recive(self):
-        reply, addr = self.fd.recvfrom(1000)
+        reply, addr = self._sock.recvfrom(1000)
         return reply
-
 
 
